7d6859b2062eb40ddbd8142670548d508802e6fe
Shantanu Kumar <976shantanu@gmail.com>
2024-11-22 13:16:38 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "hero@gmail.com";
+var accountPassword = "shan12345"
+accountCity = "jaipur"
+
+accountCity ="bihar"
+accountPassword = "shan135"
+accountEmail = "bro@gmail.com"
+//accountId = 341241;
+console.log(accountCity);
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
